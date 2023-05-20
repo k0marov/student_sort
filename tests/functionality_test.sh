756b48540beaeba74e5c575d189cf37e4580a5bb
Sam Komarov <sam@skomarov.com>
2023-05-20 15:37:41 +0300

    add removing test_res.txt to functionality test

--- tests/functionality_test.sh
+++ tests/functionality_test.sh
@@ -5,6 +5,9 @@ diff test_data/output test_res.txt
 if [[ $? -eq 0 ]]
 then
     echo "тест на проверку выхода данных прошел успешно"
+    rm test_res.txt
 else
     echo "тест на проверку выхода данных не прошел успешно"
+    exit 1
 fi
+
